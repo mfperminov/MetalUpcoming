5c65e3a3977362b63a2e9aba05a811e715e09f02
Mikhail Perminov <mfperminov@mail.ru>
2020-11-15 22:47:48 +0300

    Update versionName

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -9,7 +9,7 @@ object Versions {
     val targetSdk = 30
     val compileSdk = 30
     val versionCode = 2
-    val versionName = "1.0.1"
+    val versionName = "1.0.2"
     val androidGradlePlugin = "4.2.0-alpha16"
     val splitties = "3.0.0-alpha06"
     val lycheeVersion = "0.0.12"
